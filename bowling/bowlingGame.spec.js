3ccac41fb4857900f2019c027a671911e63b01de
Erez A. Korn <erakorn@outlook.com>
2018-12-27 10:35:10 +0200

    Second test for bowling

--- bowling/bowlingGame.spec.js
+++ bowling/bowlingGame.spec.js
@@ -1,12 +1,25 @@
 const BowlingGame = require('./bowlingGame');
 const expect = require('chai').expect;
 
+const rollMany = (game, rolls, points)  => {
+    for(let i=0; i<rolls; i++) {
+        game.roll(points);
+    }
+}
+
 describe("BowlingGame", () => {
+    let game;
+    beforeEach(() => {
+        game = new BowlingGame();
+    });
+
     it("should return 0 when gutter game", () => {
-        const game = new BowlingGame();
-        for(let i=0; i<20; i++) {
-            game.roll(0);
-        }
-            expect(game.score()).to.equal(0);
+        rollMany(game, 20, 0);
+        expect(game.score()).to.equal(0);
+    });
+
+    it('should return 150 when spare game', () => {
+        rollMany(game, 21, 5);
+        expect(game.score()).to.equal(150);
     });
 });
