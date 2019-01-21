4fa008f565127f491b0bb75251d611ecef5e4a3b
Erez A. Korn <erakorn@outlook.com>
2019-01-22 00:19:17 +0200

    Added logger

--- doubles/src/app.js
+++ doubles/src/app.js
@@ -2,5 +2,8 @@ module.exports = {
     // Express like endpoint that returns an http response
     getHelloWorld: (req, res) => {
         res.send('Hello World');
+    },
+    logger: (message) => {
+        console.log('DEBUG: ' + message);
     }
 }
