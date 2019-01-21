7be875ff40beebf4d2c1a8f2cf91868398be1dc2
Erez A. Korn <erakorn@outlook.com>
2019-01-22 01:22:52 +0200

    Added async test

--- doubles/src/app.spec.js
+++ doubles/src/app.spec.js
@@ -1,32 +1,46 @@
-const expect = require('chai').expect;
+const chai = require('chai');
+const expect = chai.expect;
 const app = require('./app');
 const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 
 describe('sinon', () => {
 
-    beforeEach(() => {
-        sinon.spy(console, 'log');
-    });
-    afterEach(() => {
-        console.log.restore();
-    });
     describe('logger', () => {
-        it ('should log to the console', () => {
-            app.logger('this is my test');
-            expect(console.log.calledOnce).to.be.true;
+        beforeEach(() => {
+            sinon.spy(console, 'log');
         });
-        it ('should log exact message to the console', () => {
+        afterEach(() => {
+            console.log.restore();
+        });
+        it('should log to the console', () => {
+            app.logger('this is my test');
+            expect(console.log).to.have.been.calledOnce;
+        });
+        it('should log exact message to the console', () => {
             const message = 'this is my message';
             app.logger(message);
-            expect(console.log.calledWith('DEBUG: ' + message)).to.be.true;
+            expect(console.log).to.have.been.calledWith('DEBUG: ' + message);
         });
     });
 
-    describe('getIndexPage', function() {
-        it('should return index page', function() {
+    describe('notAjax', () => {
+        it('should call the callback with fakeData', function (done) {
+            let spyCallback = sinon.spy(function () {
+                expect(spyCallback).to.have.been.calledWith('/home');
+                done();
+            });
+
+            app.notAjax('/home', spyCallback);
+        });
+    });
+
+    describe('getIndexPage', function () {
+        it('should return index page', function () {
             let req = {}
             let res = {
-            send: function() {}
+                send: function () { }
             }
 
             app.getHelloWorld(req, res)
