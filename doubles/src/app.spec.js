71ad5346cb2b79b8c0eaf1f35123c17ba0430d4c
Erez A. Korn <erakorn@outlook.com>
2019-01-22 00:31:14 +0200

    Added spies

--- doubles/src/app.spec.js
+++ doubles/src/app.spec.js
@@ -1,10 +1,24 @@
 const expect = require('chai').expect;
 const app = require('./app');
+const sinon = require('sinon');
 
 describe('sinon', () => {
+
+    beforeEach(() => {
+        sinon.spy(console, 'log');
+    });
+    afterEach(() => {
+        console.log.restore();
+    });
     describe('logger', () => {
         it ('should log to the console', () => {
             app.logger('this is my test');
+            expect(console.log.calledOnce).to.be.true;
+        });
+        it ('should log exact message to the console', () => {
+            const message = 'this is my message';
+            app.logger(message);
+            expect(console.log.calledWith('DEBUG: ' + message)).to.be.true;
         });
     });
 
